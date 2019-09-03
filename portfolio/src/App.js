8239057f8252eb67aeebbde463568051051c9bb7
Milena Shishkova <shishkova.milena@gmail.com>
2019-09-04 01:33:32 +0300

    initial commit

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Layout, Header, Textfield, Drawer, Navigation, Content} from 'react-mdl';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div style={{height: '300px', position: 'relative'}}>
+    <Layout fixedHeader fixedDrawer>
+        <Header title="Title">
+            <Textfield
+                value=""
+                onChange={() => {}}
+                label="Search"
+                expandable
+                expandableIcon="search"
+            />
+        </Header>
+        <Drawer title="Title">
+            <Navigation>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+            </Navigation>
+        </Drawer>
+        <Content />
+    </Layout>
+</div>
   );
 }
 
